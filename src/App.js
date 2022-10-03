98596647523f65473db7289d9e8aa46e978c3e04
ArsenLen <laurie10@mail.ru>
2022-10-03 19:58:18 +0600

    3.10.22

--- src/App.js
+++ src/App.js
@@ -10,9 +10,14 @@ function App() {
   const [users, setUsers] = useState([])
   const [name, setName] = useState('')
   const [number, setNumber] = useState('')
+  const [searchName, setSearchName] = useState('')
+  const [filteredUsers, setfilteredUsers] = useState([])
 
   useEffect(() => {
-    getUsers().then(res => setUsers(res.data))
+    getUsers().then(res => {
+      setUsers(res.data)
+      setfilteredUsers(res.data)
+    })
   }, [])
 
   const addUser = (e) => {
@@ -25,17 +30,26 @@ function App() {
       .then(res => {setUsers(users.concat(res.data))}) // для автоматического обновления на фронте
   }
 
+  const findName = (e) => {
+    setSearchName(e.target.value) // e.target.value = Ar
+    setfilteredUsers(users.filter(user => user.name.includes(e.target.value))) // массив с объектом, которые удовлетворяет поиску
+  }
+
   return (
     <div>
       <h1>Контактная книга</h1>
+      <div className='search'>
+        <h2>Поиск контактов</h2>
+        <input type="text" placeholder='поиск по имени' value={searchName} onChange={findName} />
+      </div>
       <form onSubmit={addUser}>
         <input type="text" placeholder='Введите имя' value={name} onChange={e => setName(e.target.value)} />
         <input type="number" placeholder='Введите номер' value={number} onChange={e => setNumber(e.target.value)} />
         <button type='submit'>Отправить</button>
       </form>
       <div className="contacts-wrapper">
-        {
-          users.map(user => {
+        { // [{}, {}]
+          filteredUsers.map(user => {
             return (
               <div className='contact'>
                 <h2 className='user-name'>{user.name}</h2>
