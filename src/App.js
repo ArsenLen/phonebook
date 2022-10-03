acfdc69ab551d6c330814bab0c3523f5d687d07a
ArsenLen <laurie10@mail.ru>
2022-10-03 18:30:06 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react'
 import './App.css';
+import axios from 'axios';
+
+const getUsers = () => {
+  return axios.get('http://localhost:3001/users')
+}
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [name, setName] = useState('')
+  const [number, setNumber] = useState('')
+
+  useEffect(() => {
+    getUsers().then(res => setUsers(res.data))
+  }, [])
+
+  const addUser = (e) => {
+    e.preventDefault()
+    const newUser = {
+      name,
+      number 
+    }
+    axios.post('http://localhost:3001/users', newUser)
+      .then(res => {setUsers(users.concat(res.data))}) // для автоматического обновления на фронте
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Контактная книга</h1>
+      <form onSubmit={addUser}>
+        <input type="text" placeholder='Введите имя' value={name} onChange={e => setName(e.target.value)} />
+        <input type="number" placeholder='Введите номер' value={number} onChange={e => setNumber(e.target.value)} />
+        <button type='submit'>Отправить</button>
+      </form>
+      <div className="contacts-wrapper">
+        {
+          users.map(user => {
+            return (
+              <div className='contact'>
+                <h2 className='user-name'>{user.name}</h2>
+                <p className='user-number'>{user.number}</p>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
